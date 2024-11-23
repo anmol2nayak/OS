46f8693da37fbd4120d3827fa0db7900792b5fb7
Anmol Nayak <151988024+anmol2nayak@users.noreply.github.com>
2024-11-23 23:13:58 +0530

    Delete ConcurencyeCommerce Shopping Cart directory

--- ConcurencyeCommerce Shopping Cart/lib/db.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = global.prisma ?? new PrismaClient();
-
-export default prisma;
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
